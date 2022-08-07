eb648e4b54ef96479be55026777f9760c24760fd
MD-Shibli-Mollah <md.ta.shibli@gmail.com>
2022-08-07 18:12:50 +0600

    String matching...

---
+++ b1120.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    char c;
+    string s;
+    int i, j, len, count;
+
+    while (cin>>c)
+    {
+        cin>>s;
+        len = s.size();
+        //cout<<"the size is: "<<len;
+        count = 0;
+        if (c == '0' && s[0] == '0')
+        {
+           break;
+        }
+        
+    }
+    
+
+    return 0;
+}
