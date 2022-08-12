87e868345aca6d9a8ab8d6cc3df2893129319783
MD-Shibli-Mollah <md.ta.shibli@gmail.com>
2022-08-12 19:36:04 +0600

    Accepted!!!

--- b1120.cpp
+++ b1120.cpp
@@ -4,23 +4,46 @@ using namespace std;
  
 int main() {
  
-    char c;
-    string s;
-    int i, j, len, count;
+    char c;  //Read character only
+    string s; //Read String only
+    int i, j, len, flag;
 
     while (cin>>c)
     {
         cin>>s;
         len = s.size();
         //cout<<"the size is: "<<len;
-        count = 0;
+        flag = 0; //This coiunt is to print 0 if only 0 0 as input...
         if (c == '0' && s[0] == '0')
         {
            break;
         }
-        
-    }
-    
 
+        for (i = 0; i < len; i++)
+        {
+            if (s[i] == '0')
+            {
+                if (flag !=0)
+                {
+                    cout<<s[i];
+                }
+                
+            }
+            else if (s[i] != c)
+            {
+                cout<<s[i];
+                flag = 1;
+            }
+        }  
+        if (flag == 0)
+        {
+            cout<<'0'<<endl;
+        }
+        else
+        {
+            cout<<endl;
+        }
+    
+    }
     return 0;
 }
